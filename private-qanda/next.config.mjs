e5ffcb1fcb3f7872500d968ec48db3f279fbce8b
Seshin-Mori <268gameandmusic@gmail.com>
2024-06-11 06:29:22 +0900

    ビルドルール変更

--- private-qanda/next.config.mjs
+++ private-qanda/next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export", // 静的ファイルをエクスポートするために必要
   distDir: "out", // ビルド出力先ディレクトリを指定
+  //データ不整合のレスが消える
+  //trailingSlash: true, // これを追加して全てのURLにスラッシュを追加
 };
 
 export default nextConfig;
